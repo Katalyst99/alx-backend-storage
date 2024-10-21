5cb91bcaff82cb09a2d7f43c03daeb6d4a412d25
Katalyst99 <katleho333@gmail.com>
2024-10-21 08:30:04 -0700

    9. Insert a document in Python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""The module for inserting a document in python"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Function that inserts a new document in a collection based on kwargs"""
+    if mongo_collection is None:
+        return None
+    newDoc = mongo_collection.insert_one(kwargs)
+    return newDoc
